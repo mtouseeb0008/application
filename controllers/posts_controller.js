dfc3212c2e8ac32801204d974c942756bab65d0b
kuldeep yadav <kuldeepy10459@gmail.com>
2021-11-22 00:53:00 +0530

    half work done

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require("../models/post");
+const Comment = require("../models/comment");
 module.exports.create = function (req, res) {
   Post.create(
     {
@@ -12,4 +13,20 @@ module.exports.create = function (req, res) {
       return res.redirect("back");
     }
   );
+};
+
+module.exports.destroy = function (req, res) {
+  Post.findById(req.params.id, function (err, post) {
+    //.id  means converting i din Object in to strign
+
+    if (post.student == req.user.id) {
+      post.remove();
+
+      Comment.deleteMany({ post: req.params.id }, function (err) {
+        return res.redirect("back");
+      });
+    } else {
+      return res.redirect("back");
+    }
+  });
 };
--- routes/comments.js
+++ routes/comments.js
@@ -3,5 +3,10 @@ const router = express.Router();
 const passport = require("passport");
 const commentController = require("../controllers/comment_controller");
 router.post("/create", passport.checkAuthentication, commentController.create);
+router.get(
+  "/destroy/:id",
+  passport.checkAuthentication,
+  commentController.destroy
+);
 
 module.exports = router;
